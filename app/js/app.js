349a6110ce5442badcbc259b8dbc4310e0c54cbc
Vince Suriani <vince@Vinces-MacBook-Pro.local>
2016-04-22 16:24:36 -0400

    Added Some commands to detect processes

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,36 @@ closeEl.addEventListener('click', function () {
     ipc.send('close-main-window');
 });
 
+
+
+var exec = require('child_process').exec;
+/*function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+};*/
+function parseProcesses(procs) {
+
+
+	var pArray = procs.split("\n");
+
+	console.log(pArray.length);
+
+	for(x=0; x<pArray.length; x++) {
+		//if (pArray[x].indexOf("Application") > -1) {
+			//var theProc = pArray[x].split("\t");
+			console.log(pArray[x]);
+			//console.log(theProc[0]);
+		//}
+	}
+	
+}
+
+
+//OSX also has TOP and top -o cpu
+//exec("ps -ax", function(error, stdout, stderr){ parseProcesses(stdout); });
+//exec("ps aux", function(error, stdout, stderr){ parseProcesses(stdout); });
+exec("ps -axc -o comm", function(error, stdout, stderr){ parseProcesses(stdout); }); //Might not need the -a which is ALL users...
+
+//tasklist for Windows
+
+
+console.log(`This platform is ${process.platform}`);
